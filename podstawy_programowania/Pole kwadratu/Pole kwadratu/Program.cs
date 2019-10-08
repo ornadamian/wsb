8e4b0bfa100bc24bb3f9b831d953f0da50e607d0
ornadamian <damianorna@gmail.ocm>
2019-10-08 16:06:43 +0100

    Pole kwadratu v1

---
+++ podstawy_programowania/Pole kwadratu/Pole kwadratu/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pole_kwadratu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program oblicza pole prostokąta z podanych przez użytkownika danych");
+
+            Console.Write("Podaj bok a:");
+            double bok_a = double.Parse(Console.ReadLine());
+           
+
+            Console.Write("Podaj bok b:");
+            double bok_b = double.Parse(Console.ReadLine()) ;
+            
+
+
+            Console.WriteLine("Pole prostokąta wynosi: " + bok_a * bok_b);
+
+            Console.ReadKey();
+        }
+    }
+}
